bf47036514c2537a7f5c20f057ae2e06ff7abe62
LamiaHuseynova <lamia.huseynova@gmail.com>
2023-02-14 21:35:03 +0400

    plus

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { ClothesDialogComponent } from '../clothes-dialog/clothes-dialog.component';
 
 @Component({
   selector: 'app-menu',
@@ -6,5 +8,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent {
+  dropMenu = false;
+  my_drop() {
+    this.dropMenu = !this.dropMenu;
+  }
+  clothes=[
+    {title: "Dresses", img_path: "../../assets/images/dresses.PNG", article: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur omnis magnam reprehenderit possimus minima officia vel beatae distinctio impedit doloremque."  },
+    {title: "Suits", img_path: "../../assets/images/suits.JPG", article: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur omnis magnam reprehenderit possimus minima officia vel beatae distinctio impedit doloremque." },
+    {title: "Bags", img_path: "../../assets/images/bags.PNG", article: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur omnis magnam reprehenderit possimus minima officia vel beatae distinctio impedit doloremque." },
+    {title: "Coats", img_path: "../../assets/images/coats.PNG", article: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur omnis magnam reprehenderit possimus minima officia vel beatae distinctio impedit doloremque." },
+    {title: "Shoes", img_path: "../../assets/images/shoes.PNG", article: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur omnis magnam reprehenderit possimus minima officia vel beatae distinctio impedit doloremque." },
+    {title: "Accessories", img_path: "../../assets/images/accessories.JPG", article: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur omnis magnam reprehenderit possimus minima officia vel beatae distinctio impedit doloremque." },
+    {title: "Hats", img_path: "../../assets/images/hats.PNG", article: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur omnis magnam reprehenderit possimus minima officia vel beatae distinctio impedit doloremque." },
+    {title: "Sports", img_path: "../../assets/images/sports.JPG", article: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur omnis magnam reprehenderit possimus minima officia vel beatae distinctio impedit doloremque." },
+  ]
+  constructor(public dialog: MatDialog) {
 
+  }
+  
+openDialog(i: number){
+  console.log(this.clothes[i]);
+
+  let dialogRef = this.dialog.open(ClothesDialogComponent, {
+    height: '400px',
+    width: '600px',
+    data: this.clothes[i]
+  });
+}
 }
